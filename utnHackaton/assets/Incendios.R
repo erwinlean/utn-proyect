798051e52aa09aed2b31a81e99a1f89f0b6737fb
Erwin Marte <85772547+erwinlean@users.noreply.github.com>
2023-06-24 13:46:46 -0300

    Delete utnHackaton directory

--- utnHackaton/assets/Incendios.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-library(dplyr)
-datos = read.csv("Ruta")
-#Provincias  Incendios desconocidos
-ggplot(data = datos,
-       mapping = aes(x = incendio_desconocida_numero,
-                     y = incendio_provincia)) +
-       geom_point() + labs(title = 'Pronvicias-Incendio')
